5866fe3a79cd26a963d7ed0e9c89d09d24fcf674
hamsadatta <phamsa.datta98@gmail.com>
2023-12-29 13:29:42 +0100

    dmp datagen update

--- utils/generate_dmp_data.py
+++ utils/generate_dmp_data.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import re
 import numpy as np
 from pydmps.dmp_discrete import DMPs_discrete
 
@@ -10,9 +11,8 @@ class DMPDataGenerator:
         self.config = self.read_yaml_file(config_path)
         self.n_dmps = self.config['n_dmps']
         self.n_bfs= self.config['n_bfs']
-        self.input_path= current_directory + self.config['input_path']
-        self.output_path= current_directory + self.config['output_path']
-        self.input_data =  np.array(self.read_yaml_file(self.input_path)['trajectory'])
+        self.input_folder= current_directory + self.config['input_folder']
+        self.output_folder= current_directory + self.config['output_folder']
 
     def read_yaml_file(self, path):
         with open(path, 'r') as file:
@@ -26,21 +26,40 @@ class DMPDataGenerator:
         dmp = DMPs_discrete(n_dmps=self.n_dmps, n_bfs=self.n_bfs)
         dmp.imitate_path(y_des=input_data)
         dmp.goal = [trajectory[-1] for trajectory in input_data]
-        return dmp.rollout()
+        return dmp.rollout()    
 
-    def run(self):        
-        x_path = self.input_data[:, 0]
-        y_path = self.input_data[:, 1]
+    def run(self):
+        input_files = os.listdir(self.input_folder)
+        for _, input_file in enumerate(input_files):
+            match = re.search(r'\d+', input_file)
+            if match:
+                index = int(match.group())
+                print(index)
+            else:
+                print("Please check the filename.")
+            input_path = os.path.join(self.input_folder, input_file)
+            output_path = os.path.join(self.output_folder, f'trajectory_{index}.yaml')
 
-        y_track, dy_track, ddy_track, clock_track, f_track = self.process_dmp(np.array([x_path, y_path]))
-        output_data = {
-            'y_track': y_track.tolist(),
-            'dy_track': dy_track.tolist(),
-            'ddy_track': ddy_track.tolist(),
-            'clock_track': clock_track,
-            'f_track': f_track.tolist()
-        }
-        self.write_yaml_file(self.output_path, output_data)
+            input_data = np.array(self.read_yaml_file(input_path)['trajectory'])
+            x_path = input_data[:, 0]
+            y_path = input_data[:, 1]
+            z_path = input_data[:, 2]
+            roll_path = input_data[:, 3]
+            pitch_path = input_data[:, 4]
+            yaw_path = input_data[:, 5]
+
+            y_track, dy_track, ddy_track, clock_track, f_track = self.process_dmp(np.array([x_path, y_path, z_path,
+                                                                                            roll_path, pitch_path, yaw_path]))
+            output_data = {
+                'y_track': y_track.tolist(),
+                'dy_track': dy_track.tolist(),
+                'ddy_track': ddy_track.tolist(),
+                'clock_track': clock_track,
+                'f_track': f_track.tolist()
+            }
+            self.write_yaml_file(output_path, output_data)
+
+
 
 if __name__ == "__main__":
     dmp_data = DMPDataGenerator(config_path=current_directory + '/config.yaml')
